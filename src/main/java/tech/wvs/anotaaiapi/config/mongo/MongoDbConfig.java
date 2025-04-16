64f882cccf9a32d0cde82dfe0ea474f3c5800c79
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-16 11:54:10 -0300

    refactor: add MongoDB configuration file

---
+++ src/main/java/tech/wvs/anotaaiapi/config/mongo/MongoDbConfig.java
@@ -0,0 +1,22 @@
+package tech.wvs.anotaaiapi.config.mongo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDbConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoConfigure());
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoConfigure() {
+        return new
+                SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/products-catalog");
+    }
+}
