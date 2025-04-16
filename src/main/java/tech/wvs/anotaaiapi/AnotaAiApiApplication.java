dd0a687d029d5774d086d1b92a0ef263580d86f5
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-16 10:54:18 -0300

    Create spring app

---
+++ src/main/java/tech/wvs/anotaaiapi/AnotaAiApiApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.anotaaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotaAiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/anotaaiapi/AnotaAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.anotaaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
